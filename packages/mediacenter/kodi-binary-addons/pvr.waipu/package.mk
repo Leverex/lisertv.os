54b574e3707223eeb724d65978af51789efd089a
Leverex <leverex@users.noreply.github.com>
2020-07-09 21:02:07 +0000

    kodi-binary-addons: add pvr.waipu

---
+++ packages/mediacenter/kodi-binary-addons/pvr.waipu/package.mk
@@ -0,0 +1,18 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2020-present Leverex (leverex@liser.tv)
+
+PKG_NAME="pvr.waipu"
+PKG_VERSION="02b1e92"
+PKG_SHA256="9f674ca49a8f94d24dfdc672e76f07b88abb7ebb954709c4aa365036d55b983d"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/flubshi/pvr.waipu"
+PKG_URL="https://github.com/flubshi/pvr.waipu/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain kodi-platform tinyxml2"
+PKG_SECTION=""
+PKG_SHORTDESC="pvr.waipu"
+PKG_LONGDESC="pvr.waipu"
+
+PKG_IS_ADDON="yes"
+PKG_ADDON_TYPE="xbmc.pvrclient"
